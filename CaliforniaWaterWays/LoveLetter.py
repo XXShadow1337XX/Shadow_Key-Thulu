10855089f040c0b3601b224756af2530143a1a1e
XXShadow1337XX <125276289+XXShadow1337XX@users.noreply.github.com>
2023-02-24 13:05:45 +0100

    Update LoveLetter.py
    CAESER ZU UNSICHER. KEY AUF LÄNGE 2 ERHÖHT!!! LOL

--- CaliforniaWaterWays/LoveLetter.py
+++ CaliforniaWaterWays/LoveLetter.py
@@ -1,12 +1,29 @@
-def caesar_cipher(text, shift):
-    result = ""
-    for char in text:
-        if char.isupper():
-            shifted_char = chr((ord(char) + shift - 65) % 26 + 65)
-            result += shifted_char
-        elif char.islower():
-            shifted_char = chr((ord(char) + shift - 97) % 26 + 97)
-            result += shifted_char
+def vig_encode(plaintext, key):
+    plaintext = plaintext.upper()
+    key = key.upper()
+    ciphertext = ""
+    key_index = 0
+    for char in plaintext:
+        if char.isalpha():
+            shift = ord(key[key_index]) - ord('A')
+            shifted_char = chr(((ord(char) - ord('A') + shift) % 26) + ord('A'))
+            ciphertext += shifted_char
+            key_index = (key_index + 1) % len(key)
         else:
-            result += char
-    return result
+            ciphertext += char
+    return ciphertext
+
+def vig_decode(ciphertext, key):
+    ciphertext = ciphertext.upper()
+    key = key.upper()
+    plaintext = ""
+    key_index = 0
+    for char in ciphertext:
+        if char.isalpha():
+            shift = ord(key[key_index]) - ord('A')
+            shifted_char = chr(((ord(char) - ord('A') - shift + 26) % 26) + ord('A'))
+            plaintext += shifted_char
+            key_index = (key_index + 1) % len(key)
+        else:
+            plaintext += char
+    return plaintext
